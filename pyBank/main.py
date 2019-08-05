2c0c3ca7a591f091df6f902fea99387ff67039a0
Shiva-gs <53023712+Shiva-gs@users.noreply.github.com>
2019-08-04 21:27:49 -0400

    Add files via upload

--- pyBank/main.py
+++ pyBank/main.py
@@ -15,8 +15,9 @@ run_diff = 0
 prv_diff = 0
 tot_avg = 0
 revenue_avg_list = [0]
-dic_data={}
+dict_data={}
 
+# cannot re-iterate so open again since used above
 with open(file,newline="") as csv_file:
     csv_data= csv.reader(csv_file,delimiter=",")
     head = next(csv_data)
@@ -26,7 +27,7 @@ with open(file,newline="") as csv_file:
         # Get the running average in a list
         revenue_avg_list = revenue_avg_list + [float(row[1]) - prv_bal]
         #arrange the running difference and date to dictionary 
-        dic_data[float(row[1]) - prv_bal] = [row[0]] 
+        dict_data[float(row[1]) - prv_bal] = [row[0]] 
         #Get the greatest profit
         prv_bal = float(row[1])
         prv_diff = run_diff
@@ -36,9 +37,9 @@ with open(file,newline="") as csv_file:
     #find the average
     tot_avg = round(run_diff/row_cnt,2)
     print(f"The average of the changes in Profit/Losses :${tot_avg}")
-    # ''.join(dic_data) will remove the quotes and brackets
-    print (f"The greatest increase in profits (date and amount) over the entire period: {''.join(dic_data[max(revenue_avg_list)])} -- ${max(revenue_avg_list)}")
-    print (f"The greatest decrease in losses (date and amount) over the entire period: {''.join(dic_data[min(revenue_avg_list)])} -- ${min(revenue_avg_list)}")
+    # ''.join(dict_data) will remove the quotes and brackets
+    print (f"The greatest increase in profits (date and amount) over the entire period: {''.join(dict_data[max(revenue_avg_list)])} -- ${max(revenue_avg_list)}")
+    print (f"The greatest decrease in losses (date and amount) over the entire period: {''.join(dict_data[min(revenue_avg_list)])} -- ${min(revenue_avg_list)}")
 
     # Write to a file
 out_file = './analyzed_data.txt'
@@ -49,5 +50,5 @@ with open(out_file, 'w') as file:
     file.write("Total Months: %d\n" % row_cnt)
     file.write("Total Revenue: $%d\n" % prof_loss)
     file.write("Average Revenue Change $%d\n" % tot_avg)
-    file.write("Greatest Increase in Revenue: %s ($%s)\n" % (''.join(dic_data[max(revenue_avg_list)]), max(revenue_avg_list)))
-    file.write("Greatest Decrease in Revenue: %s ($%s)\n" % (''.join(dic_data[min(revenue_avg_list)]), min(revenue_avg_list)))
+    file.write("Greatest Increase in Revenue: %s ($%s)\n" % (''.join(dict_data[max(revenue_avg_list)]), max(revenue_avg_list)))
+    file.write("Greatest Decrease in Revenue: %s ($%s)\n" % (''.join(dict_data[min(revenue_avg_list)]), min(revenue_avg_list)))
