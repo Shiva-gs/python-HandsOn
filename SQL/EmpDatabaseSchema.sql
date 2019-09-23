33515c0d008c4db9e30c453ff33f38b27e9aff6a
Shiva-gs <53023712+Shiva-gs@users.noreply.github.com>
2019-09-22 22:19:18 -0400

    Add files via upload

---
+++ SQL/EmpDatabaseSchema.sql
@@ -0,0 +1,71 @@
+﻿ 
+CREATE TABLE Department (
+    dept_no varchar(50)    NOT NULL,
+    dept_name varchar(50)   NOT NULL,
+    CONSTRAINT pk_Department PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE Dept_Emp (
+    emp_no int   NOT NULL,
+    dept_no varchar(50)   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+CREATE TABLE Dept_Manager (
+    id SERIAL PRIMARY KEY ,
+    emp_no int   NOT NULL,
+    dept_no int   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+CREATE TABLE Employee (
+    emp_no int   NOT NULL,
+    birth_date date   NOT NULL,
+    first_name varchar(50)   NOT NULL,
+    last_name varchar(50)   NOT NULL,
+    gender varchar(10)   NOT NULL,
+    hire_date date   NOT NULL,
+    CONSTRAINT pk_Employee PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Salary (
+    sal_id SERIAL PRIMARY KEY ,
+    emp_no int   NOT NULL,
+    salary int   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+CREATE TABLE titles (
+    title_id SERIAL PRIMARY KEY ,
+    emp_no int   NOT NULL,
+    title varchar(150)   NOT NULL,
+    from_date date   NOT NULL,
+    to_date date   NOT NULL
+);
+
+ALTER TABLE Dept_Emp ADD CONSTRAINT fk_Dept_Emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE Dept_Emp ADD CONSTRAINT fk_Dept_Emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES Department (dept_no);
+
+ALTER TABLE Dept_Manager ADD CONSTRAINT fk_Dept_Manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE Dept_Manager ADD CONSTRAINT fk_Dept_Manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Department (dept_no);
+
+ALTER TABLE Salary ADD CONSTRAINT fk_Salary_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+
+ALTER TABLE titles ADD CONSTRAINT fk_titles_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employee (emp_no);
+ 
+
